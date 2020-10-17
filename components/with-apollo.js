2ce4dd5f723e675f30fdd58f741794e0ca66bdfd
Jasper Kennis <contact@jasperkennis.nl>
2020-10-17 16:31:49 +0200

    moved apollo host to env

--- components/with-apollo.js
+++ components/with-apollo.js
@@ -2,7 +2,7 @@ import { withApollo } from 'next-apollo';
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: process.env.APOLLO_HOST,
   cache: new InMemoryCache(),
 });
 
