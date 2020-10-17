2ba4fee127bd88ef3ccb878c1e991c6bbffa5d57
Jasper Kennis <contact@jasperkennis.nl>
2020-10-17 20:08:53 +0200

    Green count test by scafollding lines

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,20 +16,13 @@ const Home = () => (
       </h1>
 
       <div className={styles['recipes-list']} data-test-selector="recipes-list">
+        <div className={styles['recipes-list-item']} data-test-selector="recipes-list-item">Look, cook!</div>
+        <div className={styles['recipes-list-item']} data-test-selector="recipes-list-item">Look, cook!</div>
+        <div className={styles['recipes-list-item']} data-test-selector="recipes-list-item">Look, cook!</div>
+        <div className={styles['recipes-list-item']} data-test-selector="recipes-list-item">Look, cook!</div>
+        <div className={styles['recipes-list-item']} data-test-selector="recipes-list-item">Look, cook!</div>
       </div>
     </main>
-
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by
-        {' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-      </a>
-    </footer>
   </div>
 );
 
