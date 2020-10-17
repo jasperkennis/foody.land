e4cf4e4a78021599cec5481a4144edcace6ecc73
Jasper Kennis <contact@jasperkennis.nl>
2020-10-17 16:30:11 +0200

    use env source for connection and remove from git

--- data-sources/main.ts
+++ data-sources/main.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import { ApolloServer } from 'apollo-server';
 import Users from './recipes';
 
-const client = new MongoClient('mongodb://localhost:27017/test');
+const client = new MongoClient(process.env.MAIN_MONGO_DB);
 client.connect();
 
 const server = new ApolloServer({
