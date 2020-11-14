7a8018099ca3efba347f1ea036ec35fbf7215fef
Jasper Kennis <contact@jasperkennis.nl>
2020-11-14 19:54:00 +0100

    Set up everything to at least try to read data

--- components/recipe-list.tsx
+++ components/recipe-list.tsx
@@ -3,26 +3,41 @@ import gql from 'graphql-tag';
 import React from 'react';
 import styles from '../styles/RecipeList.module.scss';
 import withApollo from '../components/with-apollo';
-import { Query, Mutation } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 
-const GET_SELECTED_REPOSITORIES = gql`
-  query {
-    selectedRecipes @client
+const GET_RECIPIES = gql`
+  query GetRecipes {
+    recipes {
+      title
+    }
   }
 `;
 
-const RecipeList = () => (
-  <Query query={GET_SELECTED_REPOSITORIES}>
-    {({ data: { selectedRecipes } }) => (
-      <div className={styles['recipe-list']} data-test-selector="recipe-list">
-        <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
-        <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
-        <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
-        <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
-        <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
-      </div>
-    )}
-  </Query>
-)
+
+
+const RecipeList = () => {
+  console.log('Getting intial props')
+  const { loading, error, data } = useQuery(GET_RECIPIES);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  console.log(data)
+  return (
+    <div className={styles['recipe-list']} data-test-selector="recipe-list">
+      <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
+      <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
+      <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
+      <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
+      <div className={styles['recipe-list-item']} data-test-selector="recipe-list-item">Look, cook!</div>
+    </div>
+  )
+}
+
+RecipeList.getInitialProps = () => {
+  console.log('Getting intial props')
+  const { loading, error, data } = useQuery(GET_RECIPIES);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  console.log(data)
+}
 
 export default withApollo({ ssr: true })(RecipeList);
--- etc/apollo-client.ts
+++ etc/apollo-client.ts
@@ -1,40 +1,8 @@
-import { HttpLink } from 'apollo-boost'
-import fetch from 'isomorphic-fetch'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloLink } from 'apollo-link';
-import { withClientState } from 'apollo-link-state';
-import ApolloClient from 'apollo-client'
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const cache = new InMemoryCache()
-
-const initialState = {
-  promotedRecipes: [
-    'Beer',
-    'Bread',
-    'Cake',
-    'Soup',
-    'Ham',
-  ]
-}
-
-const stateLink = withClientState({
-  cache,
-  defaults: initialState,
-  resolvers: {},
-});
-
-const httpLink = new HttpLink({
-  uri: process.env.APOLLO_HOST,
-  fetch,
-  // headers: {
-  //   authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
-  // },
-});
-
-const link = ApolloLink.from([stateLink, httpLink]);
+const cache = new InMemoryCache({})
 
 export default new ApolloClient({
-  link,
+  uri: process.env.APOLLO_HOST,
   cache,
 });
-
