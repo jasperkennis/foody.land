1ee47c9d92524e48e8f51a28e6974ef4b3fe7108
Jasper Kennis <contact@jasperkennis.nl>
2020-12-13 16:27:44 +0100

    make apollo client use the env

--- fl-site/etc/apollo-client.ts
+++ fl-site/etc/apollo-client.ts
@@ -1,12 +1,10 @@
-// import dotenv from 'dotenv'
-// dotenv.config()
+import dotenv from 'dotenv'
+dotenv.config()
 
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const cache = new InMemoryCache({})
-
 export default new ApolloClient({
-  // uri: 'apollo:4000',
-  uri: process.env.APOLLO_SERVER,
-  cache,
+  uri: 'apollo:4000',
+  // uri: process.env.APOLLO_SERVER,
+  cache: new InMemoryCache({}),
 })
